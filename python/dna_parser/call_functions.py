bc26f7fb18884a7e615d08e5f85cb0f629b24069
Matthieu Vilain <matthieuvilain@MacBook-Pro-de-Matthieu.local>
2023-05-06 16:33:56 -0400

    reset version to 0.1.0 to release to pypi

--- python/dna_parser/call_functions.py
+++ python/dna_parser/call_functions.py
@@ -1,15 +1,15 @@
 from .dna_parser import *
 
 
-def onehot_encoding(sequences, pad_type= "after", pad_length= 0, njobs= 1):
+def onehot_encoding(sequences, pad_type= "after", pad_length= 0, n_jobs= 1):
 
-    return onehot_encoding_rust(sequences, pad_type, pad_length, njobs)
+    return onehot_encoding_rust(sequences, pad_type, pad_length, n_jobs)
 
 
 
-def ordinal_encoding(sequences, pad_type= "after", pad_length= 0, njobs= 1):
+def ordinal_encoding(sequences, pad_type= "after", pad_length= 0, n_jobs= 1):
 
-    return ordinal_encoding_rust(sequences, pad_type, pad_length, njobs)
+    return ordinal_encoding_rust(sequences, pad_type, pad_length, n_jobs)
 
 
 def random_seq(length, nb_of_seq, seq_type= "dna", n_jobs= 1):
