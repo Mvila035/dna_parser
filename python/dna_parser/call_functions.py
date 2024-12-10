5b4f2ce73094f2eb1f8edf1873a7ecaa8a8e94fc
Matthieu Vilain <mvila035@uottawa.ca>
2024-12-10 12:27:43 -0500

    feat: Adds Fickett score encoding to the library.
    Adds Fickett score encoding under the fickett_score() function as described in James W. Fickett, Recognition of protein coding regions in DNA sequences, Nucleic Acids Research, Volume 10, Issue 17, 11 September 1982, Pages 5303–5318, https://doi.org/10.1093/nar/10.17.5303
    Adds phf as a dependencie to create static lookup tables for the parameters and probability values needed to compute Fickett scores.
    Adds tests under tests/test_fickett.py

--- python/dna_parser/call_functions.py
+++ python/dna_parser/call_functions.py
@@ -38,3 +38,7 @@ def dna_encoding(sequences, pad_type= "after", pad_length= -2, n_jobs= 1):
 def dna_walk(sequences, pad_type= "after", pad_length= -2, n_jobs= 1):
 
     return dna_walk_rust(sequences, pad_type, pad_length, n_jobs)
+
+def fickett_score(sequences, n_jobs= 1):
+    
+    return fickett_score_rust(sequences, n_jobs)
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub mod zcurve;
 pub mod chaos_game;
 pub mod eiip;
 pub mod dna_walk;
+pub mod fickett;
 
 
 use pyo3::prelude::*;
@@ -30,6 +31,7 @@ use zcurve::*;
 use chaos_game::*;
 use eiip::*;
 use dna_walk::*;
+use fickett::*;
 
 
 
@@ -52,6 +54,7 @@ fn dna_parser(_py: Python<'_>, m: &Bound<PyModule>)-> PyResult<()> {
     m.add_function(wrap_pyfunction!(chaos_encoding_rust,m)?)?;
     m.add_function(wrap_pyfunction!(eiip_encoding_rust,m)?)?;
     m.add_function(wrap_pyfunction!(dna_walk_rust,m)?)?;
+    m.add_function(wrap_pyfunction!(fickett_score_rust,m)?)?;
 
 
     Ok(())
